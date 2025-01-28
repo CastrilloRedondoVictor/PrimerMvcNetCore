ecfb52aa9f7d521d0c489e6d8c34a0000c660915
CastrilloRedondoVictor <castrilloredondovictor@gmail.com>
2025-01-28 09:31:15 +0100

    Add project files.

---
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMvcNetCore.Models;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ControladorVista()
+        {
+            ViewData["NOMBRE"] = "Víctor";
+            ViewData["EDAD"] = 22;
+            ViewBag.DiaSemana = "Lunes";
+            return View();
+        }
+
+        public IActionResult ControladorVistaModel()
+        {
+            return View(new Persona("Víctor Castrillo", "victor.castrillo@tajamar365.com", 22));
+        }
+
+        [HttpGet]
+        public IActionResult VistaControllerGet()
+        {
+            return View();
+        }
+    }
+}
---
+++ PrimerMvcNetCore/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿namespace PrimerMvcNetCore.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public int Edad { get; set; }
+
+        public Persona(string nombre, string email, int edad) {
+            this.Nombre = nombre;
+            this.Email = email;
+            this.Edad = edad;
+        }
+    }
+}
